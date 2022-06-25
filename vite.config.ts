b1b5f9f5df69544626a31e021bdb92cd8cf1a8ef
Shawn Lee <changoloco.evg@gmail.com>
2022-06-25 16:07:54 -0500

    :heavy_plus_sign: chore(deps): add vite-plugin-windicss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import solid from "vite-plugin-solid";
+import windicss from "vite-plugin-windicss";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), windicss()],
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
