d5b61f5a090ece7fdb9d728d59cc7d085a7375d3
Shawn Lee <changoloco.evg@gmail.com>
2022-06-25 18:58:08 -0500

    :sparkles: feat: finish component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
-// prettier-ignore
+import { iconCart, imageProductDesktop, imageProductMobile } from "./assets";
+
 export default function App() {
   return (
-    <div>
-      Preview
-
-      Gabrielle Essence Eau De Parfum
-
-      A floral, solar and voluptuous interpretation composed by Olivier Polge, 
-      Perfumer-Creator for the House of CHANEL.
-
-      $149.99
-      $169.99
-
-      Add to Cart
-    </div>
+    <main class="max-w-345px mx-auto | lg:max-w-2xl">
+      <article class="grid lg:grid-cols-2 | bg-white rounded-xl overflow-hidden shadow-md">
+        <picture>
+          <source media="(min-width: 1024px)" srcset={imageProductDesktop} />
+          <img src={imageProductMobile} alt="" width="345" height="242" />
+        </picture>
+        <div class="p-6 | flex flex-col gap-3 | lg:gap-6">
+          <header class="lg:space-y-5">
+            <span class="uppercase text-sm tracking-widest">Perfume</span>
+            <h1 class="text-blue-dark text-3xl font-fraunces | lg:text-5xl">
+              Gabrielle Essence Eau De Parfum
+            </h1>
+          </header>
+          <p class="text-sm leading-relaxed | lg:text-base">
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+          <div class="w-full mt-5 | grid gap-4 | lg:(mt-auto gap-6)">
+            <div class="flex items-center gap-3 | font-fraunces | lg:gap-9">
+              <span class="text-3xl text-viridian | lg:text-4xl">$149.99</span>
+              <span class="text-xs font-montserrat font-bold line-through | lg:text-base">
+                $169.99
+              </span>
+            </div>
+            <button class="py-3 | flex items-center justify-center gap-3 | bg-viridian rounded-md shadow-md text-cream outline-none | hover:bg-viridian-dark focus:(bg-viridian-dark ring-3 ring-viridian/50)">
+              <img src={iconCart} alt="" />
+              <span class="text-sm font-bold">Add to Cart</span>
+            </button>
+          </div>
+        </div>
+      </article>
+    </main>
   );
 }
---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+export { default as iconCart } from "./icon-cart.svg";
+export { default as imageProductDesktop } from "./image-product-desktop.jpg";
+export { default as imageProductMobile } from "./image-product-mobile.jpg";
